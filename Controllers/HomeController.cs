39c330e4675c32c2a7ac8b5221c7c880db02b6a0
Vinh Vu <53502920+vinhvu01@users.noreply.github.com>
2024-04-27 01:44:07 +0700

    Init project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WeatherForecast.Models;
+using WeatherForecast.Services;
+
+namespace WeatherForecast.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private static readonly string ApiKey = "";
+
+        public RestService _rs { get; set; }
+        
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _rs = new RestService();
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Index Request");
+            return View();
+        }
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult WeatherData(string id)
+        {
+            string url = $"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{id}?key={ApiKey}";
+            WeatherData results = _rs.GetWeatherData(url).Result;
+
+            return View(results);
+        }
+    }
+}
---
+++ Models/WeatherData.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace WeatherForecast.Models
+{
+    public class WeatherData
+    {
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("days")]
+        public List<Days> Days { get; set; }
+    }
+
+    public class Days
+    {
+        [JsonProperty("datetime")]
+        public string DateTime { get; set; }
+
+        [JsonProperty("windgust")]
+        public long Windgust { get; set; }
+
+        [JsonProperty("windspeed")]
+        public long WindSpeed { get; set; }
+
+        [JsonProperty("tempmin")]
+        public double TempMin { get; set; }
+
+        [JsonProperty("temp")]
+        public double Temp { get; set; }
+
+        [JsonProperty("tempmax")]
+        public double TempMax { get; set; }
+
+        [JsonProperty("feelslike")]
+        public double FeelsLike { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("pressure")]
+        public string Pressure { get; set; }
+
+        [JsonProperty("humidity")]
+        public string Humidity { get; set; }
+    }
+}
