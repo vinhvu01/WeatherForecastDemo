a97052df9661f1075dad6741582ef974ae3abbbf
Vinh Vu <53502920+vinhvu01@users.noreply.github.com>
2024-04-28 02:19:08 +0700

    Add more request in forecast page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WeatherForecast.Models;
 using WeatherForecast.Services;
+using static System.Net.WebRequestMethods;
 
 namespace WeatherForecast.Controllers
 {
@@ -10,7 +12,9 @@ namespace WeatherForecast.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        private static readonly string ApiKey = "";
+        private static readonly string ApiKey = "FD6XX363KUVNCP74P5E489F8G";
+
+        private static readonly string UrlTimeLine = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
 
         public RestService _rs { get; set; }
         
@@ -33,15 +37,51 @@ namespace WeatherForecast.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
 
         public IActionResult WeatherData(string id)
         {
-            string url = $"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{id}?key={ApiKey}";
+            string url = $"{UrlTimeLine}/{id}?key={ApiKey}";
+            WeatherData results = _rs.GetWeatherData(url).Result;
+
+            return View(results);
+        }
+
+        public IActionResult WeatherDataDynamicPeriod(string id, string period)
+        {
+            string url = $"{UrlTimeLine}/{id}/{period}?key={ApiKey}";
+            WeatherData results = _rs.GetWeatherData(url).Result;
+
+            return View(results);
+        }
+
+        public IActionResult WeatherDataByDateRange(string id, string from, string to)
+        {
+            string url = $"{UrlTimeLine}/{id}/{from}/{to}?key={ApiKey}";
+            WeatherData results = _rs.GetWeatherData(url).Result;
+
+            return View(results);
+        }
+
+        public IActionResult WeatherDataByLongLatRange(string longitude, string latitude)
+        {
+            string url = $"{UrlTimeLine}/{longitude},{latitude}?key={ApiKey}";
+            WeatherData results = _rs.GetWeatherData(url).Result;
+
+            return View(results);
+        }
+
+        public IActionResult WeatherDataBySpecificTime(string id, string specificTime)
+        {
+            var datetime = DateTime.Parse(specificTime);
+            var url = $"{UrlTimeLine}/{id}/{datetime:yyyy-MM-ddTHH:mm:ss}?key={ApiKey}";
+            WeatherData results = _rs.GetWeatherData(url).Result;
+
+            return View(results);
+        }
+
+        public IActionResult WeatherDataByUnixTime(string id, string from, string to)
+        {
+            string url = $"{UrlTimeLine}/{id}/{from}/{to}?key={ApiKey}";
             WeatherData results = _rs.GetWeatherData(url).Result;
 
             return View(results);
--- Models/WeatherData.cs
+++ Models/WeatherData.cs
@@ -46,5 +46,38 @@ namespace WeatherForecast.Models
 
         [JsonProperty("humidity")]
         public string Humidity { get; set; }
+
+        [JsonProperty("hours")]
+        public List<Hours> Hours { get; set; }
+    }
+
+    public class Hours
+    {
+        [JsonProperty("datetime")]
+        public string DateTime { get; set; }
+
+        [JsonProperty("windgust")]
+        public long Windgust { get; set; }
+
+        [JsonProperty("windspeed")]
+        public long WindSpeed { get; set; }
+
+        [JsonProperty("temp")]
+        public double Temp { get; set; }
+
+        [JsonProperty("feelslike")]
+        public double FeelsLike { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("pressure")]
+        public string Pressure { get; set; }
+
+        [JsonProperty("humidity")]
+        public string Humidity { get; set; }
+
+        [JsonProperty("winddir")]
+        public string WindDir { get; set; }
     }
 }
