6b21e771eca34cf6d2fcb0d493b6440033101022
Vinh Vu <53502920+vinhvu01@users.noreply.github.com>
2024-04-30 15:01:52 +0700

    Add charts

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using WeatherForecast.Models;
 using WeatherForecast.Services;
 
 namespace WeatherForecast.Controllers
@@ -44,8 +48,21 @@ namespace WeatherForecast.Controllers
         {
             var url = $"{UrlTimeLine}/{id}?key={ApiKey}";
             var results = _rs.GetWeatherData(url).Result;
+            var temp = new List<DataPoint>();
+            var tempMax = new List<DataPoint>();
+            var tempMin = new List<DataPoint>();
 
-            return View(results);
+			foreach (var day in results.Days)
+            {
+	            temp.Add(new DataPoint(day.DateTime, double.Parse(day.Temp)));
+	            tempMax.Add(new DataPoint(day.DateTime, double.Parse(day.TempMax)));
+	            tempMin.Add(new DataPoint(day.DateTime, double.Parse(day.TempMin)));
+			}
+			
+			ViewBag.Temp = JsonConvert.SerializeObject(temp);
+            ViewBag.TempMax = JsonConvert.SerializeObject(tempMax);
+            ViewBag.TempMin = JsonConvert.SerializeObject(tempMin);
+			return View(results);
         }
 
         public IActionResult WeatherDataDynamicPeriod(string id, string period)
@@ -61,8 +78,10 @@ namespace WeatherForecast.Controllers
         {
             var url = $"{UrlTimeLine}/{id}/{from}/{to}?key={ApiKey}";
             var results = _rs.GetWeatherData(url).Result;
+            var feelLike = results.Days.Select(day => new DataPoint(day.DateTime, double.Parse(day.FeelsLike))).ToList();
 
-            return View(results);
+            ViewBag.Feelslike = JsonConvert.SerializeObject(feelLike);
+			return View(results);
         }
 
         public IActionResult WeatherDataByLongLatRange(string longitude, string latitude)
